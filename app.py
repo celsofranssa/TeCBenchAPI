148ab12158cfb849d5066d5b7a9932e83ed94e8e
Celso <c.el@live.com>
2023-01-22 21:31:05 -0300

    feat: add flask.

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 import flask
 from redis import Redis
@@ -18,34 +17,37 @@ max_length = 256
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 
 # labels
-labels = ['alimentos','combustíveis','finanças','habitação','produtos','publicidade','saúde','serviços públicos e privados','serviços regulamentados pela anatel']
+labels = ['alimentos', 'combustíveis', 'finanças', 'habitação', 'produtos', 'publicidade', 'saúde',
+          'serviços públicos e privados', 'serviços regulamentados pela anatel']
+
 
 @app.route('/')
 def hello():
     redis.incr('hits')
     return 'This Compose/Flask demo has been viewed %s time(s).' % redis.get('hits')
 
+
 @app.route('/predict', methods=['POST'])
 def predict():
     text = flask.request.get_json(force=True)['text']
     tokenized_text = _encode(text)
-    print(tokenized_text.shape)
-    print(tokenized_text)
     ort_inputs = {input_name: tokenized_text}
     prediction = ort_session.run(None, ort_inputs)
-    
+
     response = {}
 
     for label, score in zip(labels, prediction[0].tolist()[0]):
-        response[label]=score
+        response[label] = score
 
     return flask.jsonify(response)
+
 
 def _encode(text):
     return np.array(
         [tokenizer.encode(text=text, max_length=max_length, padding="max_length", truncation=True)],
         dtype=np.int64
-        )
-        
+    )
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
